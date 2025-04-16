0cd6082656e586815f320b9906ee1b7ff1a71a93
abimanyugithub <abimanyubima@hotmail.com>
2025-04-16 16:39:53 +0700

    fiixed bug (asgi & wsgi)

--- Project/wsgi.py
+++ Project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for qr_tmmin project.
+WSGI config for Project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr_tmmin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
 
 application = get_wsgi_application()
